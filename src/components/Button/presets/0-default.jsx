9c5506d275ef4546a24da8183c7fe240e05035a5
Uk Lee <uklee@tangunsoft.com>
2023-02-14 13:32:50 +0900

    Update 0-default.jsx

--- src/components/Button/presets/0-default.jsx
+++ src/components/Button/presets/0-default.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Button from '../Button.tsx';
+import Button from '../Button';
 
 export default (
   <Button uxpId="button-1" primary={true} size="large">Click me</Button>
